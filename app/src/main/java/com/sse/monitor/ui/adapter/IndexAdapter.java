bda6b2d11d9d8a91d2f14552e461ddee0a25fac1
Eric Chen <454057760@qq.com>
2016-10-19 17:25:03 +0800

    Update MarketFragment

---
+++ app/src/main/java/com/sse/monitor/ui/adapter/IndexAdapter.java
@@ -0,0 +1,92 @@
+package com.sse.monitor.ui.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.sse.monitor.R;
+import com.sse.monitor.bean.IndexBean;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import butterknife.BindColor;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Eric on 2016/10/19.
+ */
+public class IndexAdapter extends RecyclerView.Adapter<IndexAdapter.IndexViewHolder> {
+    private List<IndexBean> mIndexData;
+    private LayoutInflater mLayoutInflater;
+    private Context mContext;
+
+    @Inject
+    public IndexAdapter(Context context) {
+        this.mLayoutInflater =
+                (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.mIndexData = Collections.emptyList();
+        this.mContext = context;
+    }
+
+    public void setIndexData(List<IndexBean> indexData) {
+        this.mIndexData = indexData;
+        this.notifyDataSetChanged();
+    }
+
+    @Override
+    public IndexViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = this.mLayoutInflater.inflate(R.layout.item_index, parent, false);
+        //View view = LayoutInflater.from(mContext).inflate(R.layout.item_index,parent,false);
+        return new IndexViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(IndexViewHolder holder, int position) {
+        Log.d("IndexAdapter","onBindViewHolder position:"+position);
+        IndexBean indexBean = mIndexData.get(position);
+        holder.tvTitle.setText(indexBean.getSymbol());
+        holder.tvIndex.setText(String.format("%.2f", indexBean.getIndex()));
+        holder.tvPrice.setText(String.format("%.2f", indexBean.getPrice()));
+        holder.tvRate.setText(String.format("%.2f", indexBean.getRate()) + "%");
+        if(indexBean.getRate()>0){
+            holder.tvIndex.setTextColor(mContext.getResources().getColor(R.color.red));
+            holder.tvPrice.setTextColor(mContext.getResources().getColor(R.color.red));
+            holder.tvRate.setTextColor(mContext.getResources().getColor(R.color.red));
+            holder.tvPrice.setText("+"+holder.tvPrice.getText());
+            holder.tvRate.setText("+"+holder.tvRate.getText());
+        }else if(indexBean.getRate()==0){
+            holder.tvIndex.setTextColor(mContext.getResources().getColor(R.color.black));
+            holder.tvPrice.setTextColor(mContext.getResources().getColor(R.color.black));
+            holder.tvRate.setTextColor(mContext.getResources().getColor(R.color.black));
+        }else{
+            holder.tvIndex.setTextColor(mContext.getResources().getColor(R.color.green));
+            holder.tvPrice.setTextColor(mContext.getResources().getColor(R.color.green));
+            holder.tvRate.setTextColor(mContext.getResources().getColor(R.color.green));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mIndexData.size();
+    }
+
+    static class IndexViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.item_index_tv_title) TextView tvTitle;
+        @BindView(R.id.item_index_tv_index) TextView tvIndex;
+        @BindView(R.id.item_index_tv_price) TextView tvPrice;
+        @BindView(R.id.item_index_tv_rate) TextView tvRate;
+
+        public IndexViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
--- app/src/main/java/com/sse/monitor/ui/fragment/MarketFragment.java
+++ app/src/main/java/com/sse/monitor/ui/fragment/MarketFragment.java
@@ -1,8 +1,14 @@
 package com.sse.monitor.ui.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.GridLayout;
 import android.widget.TextView;
 
@@ -11,20 +17,23 @@ import com.shiki.utils.StringUtils;
 import com.sse.monitor.R;
 import com.sse.monitor.bean.IndexBean;
 import com.sse.monitor.core.BaseFragment;
+import com.sse.monitor.di.components.MainComponent;
 import com.sse.monitor.mms.MmsApi;
 import com.sse.monitor.mms.MmsConstants;
+import com.sse.monitor.ui.adapter.IndexAdapter;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import javax.inject.Inject;
+
 import butterknife.BindColor;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import okhttp3.Response;
 import rx.Observable;
-import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.functions.Func1;
@@ -36,53 +45,14 @@ import rx.schedulers.Schedulers;
 public class MarketFragment extends BaseFragment {
     private static final String TAG = "MarketFragment";
 
-    @BindView(R.id.main_index_market_sh_0000001)
-    GridLayout mSH000001GL;
-    @BindView(R.id.main_index_market_sz_399001)
-    GridLayout mSZ399001GL;
-    @BindView(R.id.main_index_market_sz_399006)
-    GridLayout mSZ399006GL;
-    @BindView(R.id.main_index_market_sh_0000300)
-    GridLayout mSH000300GL;
-    @BindView(R.id.main_index_market_sh_0000016)
-    GridLayout mSH000016GL;
-    @BindView(R.id.main_index_market_sh_0000905)
-    GridLayout mSH000905GL;
-
-    TextView mSH000001TvTitle;
-    TextView mSH000001TvIndex;
-    TextView mSH000001TvUpdate;
-    TextView mSH000001TvPercent;
-
-    TextView mSZ399001TvTitle;
-    TextView mSZ399001TvIndex;
-    TextView mSZ399001TvUpdate;
-    TextView mSZ399001TvPercent;
-
-    TextView mSZ399006TvTitle;
-    TextView mSZ399006TvIndex;
-    TextView mSZ399006TvUpdate;
-    TextView mSZ399006TvPercent;
-
-    TextView mSH000300TvTitle;
-    TextView mSH000300TvIndex;
-    TextView mSH000300TvUpdate;
-    TextView mSH000300TvPercent;
-
-    TextView mSH000016TvTitle;
-    TextView mSH000016TvIndex;
-    TextView mSH000016TvUpdate;
-    TextView mSH000016TvPercent;
-
-    TextView mSH000905TvTitle;
-    TextView mSH000905TvIndex;
-    TextView mSH000905TvUpdate;
-    TextView mSH000905TvPercent;
-
     @BindColor(R.color.red)
     int RED;
     @BindColor(R.color.green)
     int GREEN;
+    @BindView(R.id.market_index_rv_content)
+    RecyclerView mMarketIndexRvContent;
+    @Inject
+    IndexAdapter mIndexAdapter;
 
 
     @Override
@@ -121,15 +91,15 @@ public class MarketFragment extends BaseFragment {
                     @Override
                     public List<IndexBean> call(String s) {
                         List<IndexBean> indexBeanList = new ArrayList<>();
-                        if(!StringUtils.isEmpty(s)){
+                        if (!StringUtils.isEmpty(s)) {
                             String[] strs = s.split(";");
-                            for (String str : strs){
-                                if(str.indexOf("=\"")>0){
+                            for (String str : strs) {
+                                if (str.indexOf("=\"") > 0) {
                                     IndexBean indexBean = new IndexBean();
-                                    indexBean.setSecurityId(str.substring(str.lastIndexOf("_")+1,str.indexOf("=")));
-                                    str = str.substring(str.indexOf("=\"")+2,str.lastIndexOf("\""));
+                                    indexBean.setSecurityId(str.substring(str.lastIndexOf("_") + 1, str.indexOf("=")));
+                                    str = str.substring(str.indexOf("=\"") + 2, str.lastIndexOf("\""));
                                     String[] contents = str.split(",");
-                                    if(contents.length >= 6){
+                                    if (contents.length >= 6) {
                                         indexBean.setSymbol(contents[0]);
                                         indexBean.setIndex(Double.valueOf(contents[1]));
                                         indexBean.setPrice(Double.valueOf(contents[2]));
@@ -148,105 +118,7 @@ public class MarketFragment extends BaseFragment {
                 .subscribe(new Action1<List<IndexBean>>() {
                     @Override
                     public void call(List<IndexBean> indexBeenList) {
-                        for (IndexBean indexBean : indexBeenList){
-                            if(indexBean.getSecurityId().equals(MmsConstants.SH_000001)){
-                                mSH000001TvTitle.setText(indexBean.getSymbol());
-                                mSH000001TvIndex.setText(String.format("%.2f", indexBean.getIndex()));
-                                if(indexBean.getRate()>0){
-                                    mSH000001TvIndex.setTextColor(RED);
-                                    mSH000001TvPercent.setTextColor(RED);
-                                    mSH000001TvUpdate.setTextColor(RED);
-                                    mSH000001TvPercent.setText("+"+indexBean.getRate()+"%");
-                                    mSH000001TvUpdate.setText("+"+indexBean.getPrice()+"");
-                                }else{
-                                    mSH000001TvIndex.setTextColor(GREEN);
-                                    mSH000001TvPercent.setTextColor(GREEN);
-                                    mSH000001TvUpdate.setTextColor(GREEN);
-                                    mSH000001TvPercent.setText(indexBean.getRate()+"%");
-                                    mSH000001TvUpdate.setText(indexBean.getPrice()+"");
-                                }
-                            }else if(indexBean.getSecurityId().equals(MmsConstants.SH_000300)){
-                                mSH000300TvTitle.setText(indexBean.getSymbol());
-                                mSH000300TvIndex.setText(String.format("%.2f", indexBean.getIndex()));
-                                if(indexBean.getRate()>0){
-                                    mSH000300TvIndex.setTextColor(RED);
-                                    mSH000300TvPercent.setTextColor(RED);
-                                    mSH000300TvUpdate.setTextColor(RED);
-                                    mSH000300TvPercent.setText("+"+indexBean.getRate()+"%");
-                                    mSH000300TvUpdate.setText("+"+indexBean.getPrice()+"");
-                                }else{
-                                    mSH000300TvIndex.setTextColor(GREEN);
-                                    mSH000300TvPercent.setTextColor(GREEN);
-                                    mSH000300TvUpdate.setTextColor(GREEN);
-                                    mSH000300TvPercent.setText(indexBean.getRate()+"%");
-                                    mSH000300TvUpdate.setText(indexBean.getPrice()+"");
-                                }
-                            }else if(indexBean.getSecurityId().equals(MmsConstants.SH_000016)){
-                                mSH000016TvTitle.setText(indexBean.getSymbol());
-                                mSH000016TvIndex.setText(String.format("%.2f", indexBean.getIndex()));
-                                if(indexBean.getRate()>0){
-                                    mSH000016TvIndex.setTextColor(RED);
-                                    mSH000016TvPercent.setTextColor(RED);
-                                    mSH000016TvUpdate.setTextColor(RED);
-                                    mSH000016TvPercent.setText("+"+indexBean.getRate()+"%");
-                                    mSH000016TvUpdate.setText("+"+indexBean.getPrice()+"");
-                                }else{
-                                    mSH000016TvIndex.setTextColor(GREEN);
-                                    mSH000016TvPercent.setTextColor(GREEN);
-                                    mSH000016TvUpdate.setTextColor(GREEN);
-                                    mSH000016TvPercent.setText(indexBean.getRate()+"%");
-                                    mSH000016TvUpdate.setText(indexBean.getPrice()+"");
-                                }
-                            }else if(indexBean.getSecurityId().equals(MmsConstants.SH_000905)){
-                                mSH000905TvTitle.setText(indexBean.getSymbol());
-                                mSH000905TvIndex.setText(String.format("%.2f", indexBean.getIndex()));
-                                if(indexBean.getRate()>0){
-                                    mSH000905TvIndex.setTextColor(RED);
-                                    mSH000905TvPercent.setTextColor(RED);
-                                    mSH000905TvUpdate.setTextColor(RED);
-                                    mSH000905TvPercent.setText("+"+indexBean.getRate()+"%");
-                                    mSH000905TvUpdate.setText("+"+indexBean.getPrice()+"");
-                                }else{
-                                    mSH000905TvIndex.setTextColor(GREEN);
-                                    mSH000905TvPercent.setTextColor(GREEN);
-                                    mSH000905TvUpdate.setTextColor(GREEN);
-                                    mSH000905TvPercent.setText(indexBean.getRate()+"%");
-                                    mSH000905TvUpdate.setText(indexBean.getPrice()+"");
-                                }
-                            }else if(indexBean.getSecurityId().equals(MmsConstants.SZ_399001)){
-                                mSZ399001TvTitle.setText(indexBean.getSymbol());
-                                mSZ399001TvIndex.setText(String.format("%.2f", indexBean.getIndex()));
-                                if(indexBean.getRate()>0){
-                                    mSZ399001TvIndex.setTextColor(RED);
-                                    mSZ399001TvPercent.setTextColor(RED);
-                                    mSZ399001TvUpdate.setTextColor(RED);
-                                    mSZ399001TvPercent.setText("+"+indexBean.getRate()+"%");
-                                    mSZ399001TvUpdate.setText("+"+indexBean.getPrice()+"");
-                                }else{
-                                    mSZ399001TvIndex.setTextColor(GREEN);
-                                    mSZ399001TvPercent.setTextColor(GREEN);
-                                    mSZ399001TvUpdate.setTextColor(GREEN);
-                                    mSZ399001TvPercent.setText(indexBean.getRate()+"%");
-                                    mSZ399001TvUpdate.setText(indexBean.getPrice()+"");
-                                }
-                            }else if(indexBean.getSecurityId().equals(MmsConstants.SZ_399006)){
-                                mSZ399006TvTitle.setText(indexBean.getSymbol());
-                                mSZ399006TvIndex.setText(String.format("%.2f", indexBean.getIndex()));
-                                if(indexBean.getRate()>0){
-                                    mSZ399006TvIndex.setTextColor(RED);
-                                    mSZ399006TvPercent.setTextColor(RED);
-                                    mSZ399006TvUpdate.setTextColor(RED);
-                                    mSZ399006TvPercent.setText("+"+indexBean.getRate()+"%");
-                                    mSZ399006TvUpdate.setText("+"+indexBean.getPrice()+"");
-                                }else{
-                                    mSZ399006TvIndex.setTextColor(GREEN);
-                                    mSZ399006TvPercent.setTextColor(GREEN);
-                                    mSZ399006TvUpdate.setTextColor(GREEN);
-                                    mSZ399006TvPercent.setText(indexBean.getRate()+"%");
-                                    mSZ399006TvUpdate.setText(indexBean.getPrice()+"");
-                                }
-                            }
-                        }
+                        mIndexAdapter.setIndexData(indexBeenList);
                     }
                 });
     }
@@ -258,39 +130,19 @@ public class MarketFragment extends BaseFragment {
 
     @Override
     protected void initViews(View self, Bundle savedInstanceState) {
-        mSH000001TvTitle = ButterKnife.findById(mSH000001GL,R.id.item_market_tv_title);
-        mSH000001TvIndex = ButterKnife.findById(mSH000001GL,R.id.item_market_tv_index);
-        mSH000001TvUpdate = ButterKnife.findById(mSH000001GL,R.id.item_market_tv_update);
-        mSH000001TvPercent = ButterKnife.findById(mSH000001GL,R.id.item_market_tv_percent);
 
-        mSZ399001TvTitle = ButterKnife.findById(mSZ399001GL,R.id.item_market_tv_title);
-        mSZ399001TvIndex = ButterKnife.findById(mSZ399001GL,R.id.item_market_tv_index);
-        mSZ399001TvUpdate = ButterKnife.findById(mSZ399001GL,R.id.item_market_tv_update);
-        mSZ399001TvPercent = ButterKnife.findById(mSZ399001GL,R.id.item_market_tv_percent);
-
-        mSZ399006TvTitle = ButterKnife.findById(mSZ399006GL,R.id.item_market_tv_title);
-        mSZ399006TvIndex = ButterKnife.findById(mSZ399006GL,R.id.item_market_tv_index);
-        mSZ399006TvUpdate = ButterKnife.findById(mSZ399006GL,R.id.item_market_tv_update);
-        mSZ399006TvPercent = ButterKnife.findById(mSZ399006GL,R.id.item_market_tv_percent);
-
-        mSH000300TvTitle = ButterKnife.findById(mSH000300GL,R.id.item_market_tv_title);
-        mSH000300TvIndex = ButterKnife.findById(mSH000300GL,R.id.item_market_tv_index);
-        mSH000300TvUpdate = ButterKnife.findById(mSH000300GL,R.id.item_market_tv_update);
-        mSH000300TvPercent = ButterKnife.findById(mSH000300GL,R.id.item_market_tv_percent);
-
-        mSH000016TvTitle = ButterKnife.findById(mSH000016GL,R.id.item_market_tv_title);
-        mSH000016TvIndex = ButterKnife.findById(mSH000016GL,R.id.item_market_tv_index);
-        mSH000016TvUpdate = ButterKnife.findById(mSH000016GL,R.id.item_market_tv_update);
-        mSH000016TvPercent = ButterKnife.findById(mSH000016GL,R.id.item_market_tv_percent);
-
-        mSH000905TvTitle = ButterKnife.findById(mSH000905GL,R.id.item_market_tv_title);
-        mSH000905TvIndex = ButterKnife.findById(mSH000905GL,R.id.item_market_tv_index);
-        mSH000905TvUpdate = ButterKnife.findById(mSH000905GL,R.id.item_market_tv_update);
-        mSH000905TvPercent = ButterKnife.findById(mSH000905GL,R.id.item_market_tv_percent);
+        mMarketIndexRvContent.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        mMarketIndexRvContent.setAdapter(mIndexAdapter);
     }
 
     @Override
     protected void initListeners() {
 
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getComponent(MainComponent.class).inject(this);
     }
 }
